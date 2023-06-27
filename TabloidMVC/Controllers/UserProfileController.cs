03c88615939e6c4378486c5f6376f5866a580645
Curtis Webb <curtwebb1194@@gmail.com>
2023-06-27 14:31:20 -0500

    UserProfile

---
+++ TabloidMVC/Controllers/UserProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        // Add the UserProfile repository dependency
+        private readonly IUserProfileRepository _userProfileRepository;
+
+        public UserProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepository = userProfileRepository;
+        }
+        public IActionResult Index()
+        {
+            var userProfiles = _userProfileRepository.GetAllUsersOrderedByDisplayName();
+            return View(userProfiles);
+        }
+
+    }
+}
