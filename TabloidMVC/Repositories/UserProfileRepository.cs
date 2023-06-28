45142eea6010f2200d9877c143485ae61150ea1e
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-06-28 11:55:56 -0400

    user profile details with default pic if no pic

--- TabloidMVC/Repositories/UserProfileRepository.cs
+++ TabloidMVC/Repositories/UserProfileRepository.cs
@@ -179,10 +179,10 @@ namespace TabloidMVC.Repositories
                             {
                                 userProfile.ImageLocation = reader.GetString(reader.GetOrdinal("ImageLocation"));
                             }
-                            //else
-                            //{
-                            //    // default image - do I do this here?
-                            //}
+                            else
+                            {
+                                userProfile.ImageLocation = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+                            }
 
                             return userProfile;
                         }
