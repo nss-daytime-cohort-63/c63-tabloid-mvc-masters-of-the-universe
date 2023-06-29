09f09dcd77f30f0bf39963ea7db48f14fb15a211
Amanda Asgari-Rad <amanda_asgarirad@yahoo.com>
2023-06-29 11:12:30 -0500

    adding a tag to a post and viewing tags on posts

---
+++ TabloidMVC/Models/PostTag.cs
@@ -0,0 +1,10 @@
+﻿namespace TabloidMVC.Models
+{
+    public class PostTag
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int TagId { get; set; }
+
+    }
+}
---
+++ TabloidMVC/Repositories/PostTagRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class PostTagRepository : BaseRepository, IPostTagRepository
+    {
+        public PostTagRepository(IConfiguration config) : base(config) { }
+
+        public void CreatePostTag(PostTag postTag)
+        {
+            using (SqlConnection conn = Connection)
+            { 
+                conn.Open(); 
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO PostTag
+                        (PostId, TagId)
+                        OUTPUT INSERTED.ID
+                        VALUES
+                        (@PostId, @TagId)";
+
+                    cmd.Parameters.AddWithValue("@PostId", postTag.PostId);
+                    cmd.Parameters.AddWithValue("@TagId", postTag.TagId);
+
+                    postTag.Id = (int)cmd.ExecuteScalar();
+                }
+            
+            }
+        }
+    }
+}
