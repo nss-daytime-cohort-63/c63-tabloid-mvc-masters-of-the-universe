f52545f3be0b0d1d4929abea503e1de25631f743
Conor Getty <conorgetty@gmail.com>
2023-06-30 14:52:12 -0500

    finished feature

---
+++ TabloidMVC/Models/ViewModels/FilterPostByAuthorViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class FilterPostByAuthorViewModel
+    {
+        public List<Post> Posts { get; set; }
+        public List<UserProfile> AllUserProfiles { get; set; }
+    }
+}
